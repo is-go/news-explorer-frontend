70de0bd1b463020c8e86c379db1c0cd4f06ce611
Isaic Gonzalez <go.isaic@gmail.com>
2024-07-19 14:36:11 -0500

    "Init commit: Assets, structure, npm"

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import reactJsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "18.2",
+      },
+    },
+  },
+  {
+    plugins: {
+      "react-refresh": reactRefreshPlugin,
+      react: reactJsxRuntime,
+      "react-hooks": reactHooks,
+    },
+  },
+  {
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/prop-types": 0,
+    },
+  },
+  {
+    files: ["*.js", "*.jsx"],
+  },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+];
